913a5d7fa81bd0e53064a0632d7c58bdbab7e8c3
usermuser <lwrdrstv@gmail.com>
2019-03-27 02:44:42 +0500

    modified templates to inherit from base html

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.index),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.index, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
