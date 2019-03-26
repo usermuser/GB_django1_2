a2a4420a71945d15214d87e924cdc86d38f54656
usermuser <lwrdrstv@gmail.com>
2019-03-27 04:37:11 +0500

    active links now working

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.urls import resolve
 import json
 import datetime
 
 now = datetime.datetime.now()
 current_year = now.year
 
+
 def index(request):
     title = 'Главная'
+    current_url = resolve(request.path_info).url_name
     ctx = {'title': title,
-           'current_year': current_year, }
+           'current_year': current_year,
+           'current_url': current_url, }
+
     return render(request, 'mainapp/index.html', ctx)
+
 
 def products(request):
     with open ('docs/links_menu.json') as data:
@@ -21,6 +27,7 @@ def products(request):
            'current_year': current_year, }
 
     return render(request, 'mainapp/products.html', ctx)
+
 
 def contact(request):
     title = 'Контакты'
