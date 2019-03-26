7676f4ee80c569f71a80d58d34485522c54e21c0
usermuser <lwrdrstv@gmail.com>
2019-03-27 02:18:47 +0500

    added proper link to static files

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,6 +20,6 @@ import mainapp.views as mainapp
 urlpatterns = [
     path('', mainapp.index),
     path('products/', mainapp.products),
-    path('contact/', mainapp.products),
+    path('contact/', mainapp.contact),
     path('admin/', admin.site.urls),
 ]
