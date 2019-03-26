8bea63eaa906be3cb1a8069ce59aa3d8f2331742
usermuser <lwrdrstv@gmail.com>
2019-03-27 03:41:30 +0500

    passing current year to context data to all te

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 import json
+import datetime
+
+now = datetime.datetime.now()
+current_year = now.year
 
 def index(request):
     title = 'Главная'
-    ctx = {'title': title,}
+    ctx = {'title': title,
+           'current_year': current_year, }
     return render(request, 'mainapp/index.html', ctx)
 
 def products(request):
     with open ('docs/links_menu.json') as data:
         links_menu = json.load(data)
 
-    print(links_menu)
     title = 'Каталог'
     ctx = {'title': title,
            'links_menu': links_menu,
-           }
+           'current_year': current_year, }
+
     return render(request, 'mainapp/products.html', ctx)
 
 def contact(request):
     title = 'Контакты'
     ctx = {'title': title,
-           }
+           'current_year': current_year, }
+
     return render(request, 'mainapp/contact.html', ctx)
