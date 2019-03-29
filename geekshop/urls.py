26e3a93fcac82f041270668240e99ffd57971da6
usermuser <lwrdrstv@gmail.com>
2019-03-30 02:56:26 +0500

    7 done. links for products and categories are working

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path('', mainapp.index, name='main'),
-    path('products/', mainapp.products, name='products'),
+
+    # {% url 'mainapp:urlname' %}
+    # http://127.0.0.1:8000/products/
+    path('products/', include('mainapp.urls', namespace='mainapp',)),
+
+    # {% url 'mainapp:urlname' %}
+    # http://127.0.0.1:8000/category/
+    # path('category/', include('mainapp.urls', namespace='mainapp',)),
+
     path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.urls import resolve
+from django.http import HttpRequest, HttpResponseRedirect
+from .models import ProductCategory, Product
 import json
 import datetime
 
@@ -7,23 +9,39 @@ now = datetime.datetime.now()
 current_year = now.year
 
 
+def categories(request, pk=None):
+    if pk is None:
+        return HttpResponseRedirect('/')
+    else:
+        categories = ProductCategory.objects.order_by('name')
+        current_category=ProductCategory.objects.get(pk=pk)
+        ctx = {'categories':categories,
+               'current_category': current_category, }
+        return render(request, 'mainapp/category.html', ctx)
+
 def index(request):
-    title = 'Главная'
     current_url = resolve(request.path_info).url_name
+    title = 'Главная'
+
     ctx = {'title': title,
            'current_year': current_year,
-           'current_url': current_url, }
+           'current_url': current_url,
+           'products': products}
 
     return render(request, 'mainapp/index.html', ctx)
 
 
-def products(request):
-    with open ('docs/links_menu.json') as data:
-        links_menu = json.load(data)
-
+def products(request, pk=None):
     title = 'Каталог'
+    categories = ProductCategory.objects.order_by('name')
+    if pk is not None:
+        products = Product.objects.filter(pk=pk)
+    else:
+        products = Product.objects.order_by('-price')
+
     ctx = {'title': title,
-           'links_menu': links_menu,
+           'categories': categories,
+           'products':products,
            'current_year': current_year, }
 
     return render(request, 'mainapp/products.html', ctx)
@@ -31,6 +49,7 @@ def products(request):
 
 def contact(request):
     title = 'Контакты'
+
     ctx = {'title': title,
            'current_year': current_year, }
 
