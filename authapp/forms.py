ddcc5fab36af218b58bc7602dac2d7c57f8d7136
usermuser <lwrdrstv@gmail.com>
2019-04-05 00:04:43 +0500

    login function is working

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.forms import AuthenticationForm
 
-from .models import import ShopUser
+from authapp.models import ShopUser
 
 
 class ShopUserLoginForm(AuthenticationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'password')
-
 
     def __init__(self, *args, **kwargs):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
