a0b61245190ad3ac02043d4aa6e3424fd7c85598
usermuser <lwrdrstv@gmail.com>
2019-04-07 23:09:11 +0500

    lesson4 done (without custom validators)

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import ShopUser
 
@@ -12,3 +12,45 @@ class ShopUserLoginForm(AuthenticationForm):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
+                field.help_texts = ''
+
+
+        def clean_age(self):
+            data = self.cleaned_data['age']
+            if data < 18:
+                raise forms.ValidationError('Вы слишком молоды!')
+
+            return data
+
+
+class ShopUserChangeForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
+                field.help_texts = ''
+                if field_name == 'password':
+                    field.widget = forms.HiddenInput()
+
+        def clean_age(self):
+            data = self.cleaned_data['age']
+            if data < 18:
+                raise forms.ValidationError('Вы слишком молоды!')
+
+            return data
+
--- authapp/urls.py
+++ authapp/urls.py
@@ -8,4 +8,6 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('edit/', views.edit, name='edit'),
+    path('register/', views.register, name='register')
 ]
