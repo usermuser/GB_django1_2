ca9b9bb3dc7e3845ec080f47d452dc7acfb34bef
usermuser <lwrdrstv@gmail.com>
2019-05-11 23:23:00 +0500

    lesson2.2:           - signals

--- authapp/models.py
+++ authapp/models.py
@@ -3,6 +3,9 @@ from django.utils.timezone import now
 from datetime import timedelta
 from django.db import models
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 def expiration_day():
     return now() + timedelta(hours=48)
@@ -33,3 +36,11 @@ class ShopUserProfile(models.Model):
     aboutMe = models.TextField(verbose_name='о себе', max_length=512, blank=True)
     gender = models.CharField(verbose_name='пол', max_length=1,
                               choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+        else:
+            instance.shopuserprofile.save()
+
